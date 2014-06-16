e47019295ffbf406ad11059c619392d0755194c5
josuebrunel <josuebrunel@gmail.com>
2014-06-16 22:02:14 +0200

    User profile pages [OK]

--- app/views.py
+++ app/views.py
@@ -36,6 +36,7 @@ def index():
         user = user,
         posts = posts)
 
+    
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler # Tells flask that this is our login view
 def login():
@@ -49,6 +50,21 @@ def login():
 
     return render_template('login.html', title='Sign In', form=form, providers= app.config['OPENID_PROVIDERS'])
 
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname= nickname).first()
+    if user == None:
+        flash('User '+ nickname +' no found')
+        return redirect(url_for('index'))
+
+    posts = [
+        {'author': user, 'body': 'test 1'},
+        {'author': user, 'body': 'test 2'}
+    ]
+    return render_template('user.html', user= user, posts= posts)
+    
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == '':
