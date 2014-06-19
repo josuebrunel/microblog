720873496096695958e72f077678c9a9f05c72a2
josuebrunel <josuebrunel@gmail.com>
2014-06-19 17:53:11 +0200

    It's Twitter Now ^_^

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,6 @@ class EditForm(Form):
             return False
 
         return True
+
+class PostForm(Form):
+    post = TextField('post', validators= [Required('This field is required')])
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app, db, lm, oid
 from flask import render_template, flash, redirect, url_for, session, request, g
-from forms import LoginForm, EditForm
+from forms import LoginForm, EditForm, PostForm
 from models import User, Post, ROLE_USER, ROLE_ADMIN
 
 from datetime import datetime
@@ -33,24 +33,22 @@ def before_request():
         db.session.add(g.user)
         db.session.commit()
     
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = g.user
-    posts = [
-        { 
-            'author': { 'nickname': 'John' }, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': { 'nickname': 'Susan' }, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body= form.post.data, timestamp = datetime.utcnow(), author = g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+        
+    posts = g.user.followed_posts().all()
     return render_template('index.html',
         title = 'Home',
-        user = user,
+        form = form,
         posts = posts)
 
     
