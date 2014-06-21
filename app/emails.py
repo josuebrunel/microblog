baef02de6c1b03f14c58a8613152cf41a8ed62a4
josuebrunel <josuebrunel@gmail.com>
2014-06-21 10:46:20 +0200

    Threading the email notification process

--- app/emails.py
+++ app/emails.py
@@ -4,12 +4,18 @@ from flask.ext.mail import Message
 from flask import render_template
 from config import ADMINS
 
+from threading import Thread
+
+def send_async_email(msg):
+    mail.send(msg)
+
 def send_email(subject, sender, recipients, text_body, html_body):
 
     msg = Message(subject, sender, recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target = send_async_email, args = [msg]) #Threading send_email
+    thr.start()
 
 def follower_notification(followed, follower):
 
